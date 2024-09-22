9f546bc840a9b29512ccf41fd66ed9dd847d428c
Abhishek Jha <abhi.jha.cs@gmail.com>
2024-09-22 18:40:56 +0530

    Added sitemap and robots files for seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+	site: "https://abhijha.in",
+	integrations: [icon(), sitemap()],
 });
