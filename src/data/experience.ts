eb09ea401261a8044037c608e196092b98cd4c72
Abhishek Jha <abhi.jha.cs@gmail.com>
2025-07-06 14:57:30 +0530

    Updated experience section

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -2,30 +2,40 @@ import type { Props as ExperienceProps } from "../components/ExperienceSection/E
 
 export const ExperienceData: ExperienceProps[] = [
 	{
-		companyName: "Setu",
+		companyName: "Setu (Pine Labs)",
 		companyLogo: "/images/setu.jpg",
 		companyWebsite: "https://setu.co/",
-		jobTitle: "SDE 1 - Full Stack",
-		duration: "June 2023 - Present",
-		points: [],
-		techStack: [],
+		positions: [
+			{
+				jobTitle: "Software Engineer 2 - Full Stack",
+				duration: "April 2025 - Present"
+			},
+			{
+				jobTitle: "Software Engineer 1 - Full Stack",
+				duration: "June 2023 - March 2025"
+			},
+		],
 	},
 	{
 		companyName: "Microsoft",
 		companyLogo: "/images/microsoft.png",
 		companyWebsite: "https://www.microsoft.com/en-us/research/",
-		jobTitle: "Research Intern",
-		duration: "April 2023 - June 2023",
-		points: [],
-		techStack: [],
+		positions: [
+			{
+				jobTitle: "Research Intern",
+				duration: "April 2023 - June 2023"
+			},
+		],
 	},
 	{
-		companyName: "Setu",
+		companyName: "Setu (Pine Labs)",
 		companyLogo: "/images/setu.jpg",
 		companyWebsite: "https://setu.co/",
-		jobTitle: "SDE Intern - Frontend",
-		duration: "September 2022 - April 2023",
-		points: [],
-		techStack: [],
+		positions: [
+			{
+				jobTitle: "Software Engineer Intern - Frontend",
+				duration: "September 2022 - April 2023"
+			},
+		],
 	},
 ];
